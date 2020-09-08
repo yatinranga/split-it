831e81803182006486fc860c8a697615372e1b3f
yatinranga <41532056+yatinranga@users.noreply.github.com>
2020-09-08 13:36:38 +0530

    first commit of split-it

---
+++ src/main/java/com/project/splitit/SplitItApplication.java
@@ -0,0 +1,13 @@
+package com.project.splitit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplitItApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/splitit/SplitItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.splitit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
